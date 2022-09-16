0edbc9570d282dc081fdbeeb21e331f858c4e869
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-16 13:25:22 +0300

    starting stage 2

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -5,12 +5,10 @@ MESSAGE = "Type and required field validation: {} errors"
 
 def main():
 
-    database = json.loads(input())
     errors = {"bus_id" : 0, "stop_id" : 0, "stop_name" : 0, "next_stop" : 0, "stop_type" : 0, "a_time" : 0}
     error_count = 0
 
-
-    for data in database:
+    for data in json.loads(input()):
         if data["bus_id"] == None or not isinstance(data["bus_id"], int) or data["bus_id"] < 0:
             error_count += 1
             errors["bus_id"] += 1
@@ -23,18 +21,18 @@ def main():
         if data["next_stop"] == None or not isinstance(data["next_stop"], int) or data["next_stop"] < 0:
             error_count += 1
             errors["next_stop"] += 1
-        if data["stop_type"] != None:
-            if not isinstance(data["stop_type"], str) or len(data["stop_type"]) > 1:
+        if data["stop_type"] != None and data["stop_type"] != "":
+            if not isinstance(data["stop_type"], str) or len(data["stop_type"]) > 1 or not re.match("[SOF]", data["stop_type"]):
                 error_count += 1
                 errors["stop_type"] += 1
-        if data["a_time"] == None or not isinstance(data["a_time"], str) or not re.match("^\d\d:\d\d$", data["a_time"]):
+        if data["a_time"] == None or not isinstance(data["a_time"], str) or not re.match("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", data["a_time"]):
             error_count += 1
             errors["a_time"] += 1
 
-
     print(MESSAGE.format(error_count))
     for field in errors:
-        print(field, errors[field], sep=": ")
+        if errors[field] > 0:
+            print(field, errors[field], sep=": ")
 
 
 if __name__ == "__main__":
