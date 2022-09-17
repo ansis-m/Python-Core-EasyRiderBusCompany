90f5849f3327e503d5bc0f65984d9f574edb5d06
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-17 13:16:07 +0300

    completed the project

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -2,39 +2,69 @@ import json
 import re
 
 
-def minutes(time):
-    try:
-        hours_and_minutes = time.lstrip("0").split(":")
-        return int(hours_and_minutes[0]) * 60 + int(hours_and_minutes[1])
-    except:
-        print("exception")
-        return -1
+def no_start_and_end(result):
+    for line in result:
+        if not result[line].issuperset({"S", "F"}):
+            return line
+
+    return "x"
+
+def get_transfer_stops(transfers):
+    result = set()
+
+    for stop in transfers:
+        if len(transfers[stop]) > 1:
+            result.add(stop)
+
+    return result
+
 
 def main():
 
-    id = 0
-    time = 0
-    ok = True
-    next = False
+    transfer_stops = ['Elm Street', 'Sesame Street', 'Sunset Boulevard']
+    terminals = {128 : {'Sesame Street', 'Prospekt Avenue'}, 256 : {'Pilotow Street', 'Sesame Street'}, 512 : {'Bourbon Street', 'Sunset Boulevard'}}
 
-    print("Arrival time test:")
+
+    start_stops = set() # all start stops
+    finish_stops = set() # all finish stops
+    ondemand_stops = set()
+    transfers = {} # dict to find transfers
+    result = {} # check if all lines have start and finish
+
+
+
     for data in json.loads(input()):
-        if next and data["bus_id"] == id:
-            continue
-        next = False
-        if data["bus_id"] != id:
-            id = data["bus_id"]
-        elif time >= minutes(data["a_time"]):
-            i = data["bus_id"]
-            j = data["stop_name"]
-            print(f"bus_id line {i}: wrong time on station {j}")
-            ok = False
-            next = True
-            continue
-        time = minutes(data["a_time"])
+        if data["bus_id"] in result:
+            result[data["bus_id"]].add(data["stop_type"])
+        else:
+            result.update({data["bus_id"] : {data["stop_type"],}})
+        if data["stop_type"] == "S":
+            start_stops.add(data["stop_name"])
+        if data["stop_type"] == "F":
+            finish_stops.add(data["stop_name"])
+        if data["stop_type"] == "O":
+            ondemand_stops.add(data["stop_name"])
+        if data["stop_name"] in transfers:
+            transfers[data["stop_name"]].add(data["bus_id"])
+        else:
+            transfers.update({data["stop_name"] : {data["bus_id"],}})
 
-    if ok:
+
+    transfer_stops = get_transfer_stops(transfers)
+
+    bad_stops = []
+
+    for stop in ondemand_stops:
+        if stop in start_stops.union(finish_stops).union(transfer_stops):
+            bad_stops.append(stop)
+
+
+    print("On demand stops test:")
+    if len(bad_stops) == 0:
         print("OK")
+    else:
+        bad_stops.sort()
+        print("Wrong stop type:", bad_stops)
 
 
 if __name__ == "__main__":
